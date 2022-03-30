95a1a43796c59703c117c34e2e273ca01acbe636
kristinag9 <krisitsekova9@gmail.com>
2022-03-30 17:26:17 +0300

    Added index.js file

---
+++ Timer/index.js
@@ -0,0 +1,18 @@
+// console.log(this); // 'this' is the window object
+
+const durationInput = document.getElementById('duration');
+const startButton = document.getElementById('start');
+const pauseButton = document.getElementById('pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton, {
+   onStart() {
+      console.log('Timer started!');
+   },
+   onTick() {
+      console.log('Timer just ticked down!');
+   },
+   onComplete() {
+      console.log('Timer is completed!');
+   }
+});
+
